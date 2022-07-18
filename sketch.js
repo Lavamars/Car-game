83ce814d524a4062c641848c6f5e1b946de9c8d9
Lavamars <102724291+Lavamars@users.noreply.github.com>
2022-07-18 19:31:57 +1000

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -7,10 +7,10 @@ var cars = [];
 
 //BP
 function preload() {
-  backgroundImage = loadImage("./assets/background.png");
-  car1_img = loadImage("../assets/car1.png");
-  car2_img = loadImage("../assets/car2.png");
-  track = loadImage("../assets/track.jpg");
+  backgroundImage = loadImage("assets/background.png");
+  car1_img = loadImage("assets/car1.png");
+  car2_img = loadImage("assets/car2.png");
+  track = loadImage("assets/track.jpg");
 }
 
 //BP
